ca5205ae44f9d74709fe80fbeabfa5328a520225
ambrosia-fish <josefameur@gmail.com>
2024-09-13 15:44:01 -0400

    Adjustments for building to gh-pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,12 @@ import { Routes } from '@angular/router';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 import { MovieCardComponent } from './movie-card/movie-card.component';
 
+console.log('app.routes.ts is being loaded');
+
 export const routes: Routes = [
   { path: 'welcome', component: WelcomePageComponent },
   { path: 'movies', component: MovieCardComponent },
   { path: '', redirectTo: 'welcome', pathMatch: 'full' },
 ];
+
+console.log('Routes:', routes);

938ac6ca112539c975e202d07701e2431347c04a
ambrosia-fish <josefameur@gmail.com>
2024-09-13 15:10:33 -0400

    Updated movie-card to display as MatButtonModules/Icons

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { FetchApiDataService } from '../services/fetch-api-data.service';
 
 @Component({
   selector: 'app-movie-card',
   standalone: true,
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, MatCardModule, MatButtonModule, MatIconModule],
   templateUrl: './movie-card.component.html',
   styleUrls: ['./movie-card.component.scss']
 })
@@ -25,7 +27,7 @@ export class MovieCardComponent implements OnInit {
         this.movies = resp;
         console.log(this.movies);
       },
-      error: (error: any) => {
+      error: (error) => {
         console.error('Error fetching movies:', error);
       }
     });

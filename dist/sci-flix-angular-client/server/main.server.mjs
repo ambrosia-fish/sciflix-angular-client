45fbfb8abc97dc45f299d416381bf3b21c3d037f
ambrosia-fish <josefameur@gmail.com>
2024-09-16 15:13:50 -0400

    Removing dist from .gitignore

---
+++ dist/sci-flix-angular-client/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-ZUOOBJS7.mjs";import"./chunk-JCNZ7XPP.mjs";import"./chunk-WSUWRFSV.mjs";import"./chunk-DCFYTP2A.mjs";export{a as default};
---
+++ dist/sci-flix-angular-client/server/render-utils.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{j as o,k as r,l as n}from"./chunk-JCNZ7XPP.mjs";import{Ob as e}from"./chunk-WSUWRFSV.mjs";import"./chunk-DCFYTP2A.mjs";export{n as renderApplication,r as renderModule,e as \u0275Console,o as \u0275SERVER_CONTEXT};
